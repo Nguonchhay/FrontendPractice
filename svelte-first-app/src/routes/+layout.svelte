c569e5945054b14fd3e33db4c5d8ad23c334f655
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2024-05-23 17:03:45 +0700

    [SvelteFirstApp] Integrate MiniStore theme

--- svelte-first-app/src/routes/+layout.svelte
+++ svelte-first-app/src/routes/+layout.svelte
@@ -1,1 +1,14 @@
+<script>
+    import Svg from "./../components/commons/Svg.svelte";
+    import PopupSearch from "./../components/commons/PopupSearch.svelte";
+    import Header from "./../components/commons/Header.svelte";
+    import Footer from "./../components/commons/Footer.svelte";
+    import FooterBottom from "./../components/commons/FooterBottom.svelte";
+</script>
+
+<Svg />
+<PopupSearch />
+<Header />
 <slot />
+<Footer />
+<FooterBottom />
--- svelte-first-app/src/routes/+page.svelte
+++ svelte-first-app/src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import BillBoard from "./../components/commons/Billboard.svelte";
+    import CompanyService from "./../components/commons/CompanyService.svelte";
+    import PortraiteCarousel from "./../components/commons/PortraiteCarousel.svelte";
+    import DiscountProduct from "./../components/commons/DiscountProduct.svelte";
+    import LatestProduct from "./../components/commons/LatestProduct.svelte";
+    import Testimony from "./../components/commons/Testimony.svelte";
+    import Newsletter from "./../components/commons/Newsletter.svelte";
+    import HighlightCategry from "./../components/commons/HighlightCategory.svelte";
+</script>
+
+<BillBoard />
+<CompanyService />
+<PortraiteCarousel />
+<DiscountProduct />
+<LatestProduct />
+<Testimony />
+<Newsletter />
+<HighlightCategry />
