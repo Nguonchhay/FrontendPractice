5a42afad6be7944a2c950dc3818ca2ace4533665
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-05-23 19:33:24 +0700

    [x-clone] News component

--- NextJsPractice/x-clone/src/components/News.jsx
+++ NextJsPractice/x-clone/src/components/News.jsx
@@ -15,14 +15,31 @@ export default function News() {
   }, []);
 
   return (
-    <div>
+    <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 mt-2">
+      <h2 className="font-bold text-xl px-4">What's happen?</h2>
+      <ul>
       {
-        news.slice(0, articleNum).map(article => (
+        news.slice(0, articleNum).map((article, ind) => (
           <div>
-            <h2>{article.title}</h2>
+            <li key={ind}>
+              <a href={article.url} target="_blank">
+                <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-300 transition duration-200">
+                  <div className="space-y-0.5">
+                    <h5>{article.title}</h5>
+                    <p>{article.source.name}</p>
+                  </div>
+                
+                  <img src={article.urlToImage} alt="" width={70} className="rounded-xl" />
+                </div>
+              </a>
+            </li>
           </div>
         ))
       }
+      </ul>
+      <button onClick={() => setArticleNum(articleNum + 3)} className="w-full py-2 bg-gray-200 hover:bg-gray-300 transition duration-200">
+        Load more
+      </button>
     </div>
   )
 }
