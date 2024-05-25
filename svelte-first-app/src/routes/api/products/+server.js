fff982cad3b83ee6014a2839d6806f5184f16856
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-05-25 15:58:59 +0700

    [SvelteFirstApp] API route POST product

--- svelte-first-app/src/routes/api/products/+server.js
+++ svelte-first-app/src/routes/api/products/+server.js
@@ -5,3 +5,15 @@ export function GET() {
     const res = products;
     return successResponse(res, 'Product list');
 }
+
+export async function POST(requestEvent) {
+    const { request } = requestEvent;
+    const { title, unitPrice, imageUrl } = await request.json();
+    const newProduct = {
+        title,
+        unitPrice,
+        imageUrl
+    };
+    products.push(newProduct);
+    return successResponse(newProduct, 'Product is saved');
+}
